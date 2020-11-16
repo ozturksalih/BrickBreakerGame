84a4dd58ba01886a3c7c0de0731d39e0ff798526
salih <salihozturk020@gmail.com>
2020-11-16 13:46:34 +0100

    added rectangle

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Brick Breaker");
+
+        //the old one
+        primaryStage.setScene(new Scene(root, 1000, 600));
+//
+        //create a canvas
+        Canvas canvas = new Canvas(1000.0f , 600.0f);
+
+        //graphicsContext
+        GraphicsContext graphicsContext =
+                canvas.getGraphicsContext2D();
+
+        //set fill for rectangle
+        graphicsContext.setFill(Color.RED);
+        graphicsContext.fillRect(450 , 550 , 100 , 30);
+
+        //Create a group
+        Group group = new Group(canvas);
+
+        //create a scene
+        Scene scene = new Scene(group , 1000 ,600);
+
+        //set the scene
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        launch(args);
     }
 }
